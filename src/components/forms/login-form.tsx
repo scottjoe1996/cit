9d5d8c34fe37f79bdde67893b04bd808cf7bd9a8
Joseph Scott <j.scott@resilientplc.com>
2023-02-03 14:17:00 +0000

    added email validation

--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import validator from 'email-validator';
 
 import { Card, CardContent, CardHeader, TextField } from '@mui/material';
 
@@ -10,7 +11,7 @@ const LoginForm: React.FunctionComponent<LoginFormProps> = () => {
   const [emailField, setEmailField] = React.useState<Field>({ value: '', error: NO_ERROR });
 
   const handleEmailChange = React.useCallback((event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setEmailField({ value: event.currentTarget.value, error: NO_ERROR });
+    setEmailField({ value: event.currentTarget.value, error: getEmailError(event.currentTarget.value) });
   }, []);
 
   return (
@@ -30,5 +31,7 @@ const LoginForm: React.FunctionComponent<LoginFormProps> = () => {
     </Card>
   );
 };
+
+const getEmailError = (email: string) => (validator.validate(email) ? NO_ERROR : 'Invalid email address');
 
 export default LoginForm;
