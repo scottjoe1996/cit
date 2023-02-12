47fda3ba38dcc382e44c4143f12e9c9828a2691e
Joseph Scott <j.scott@resilientplc.com>
2023-02-12 19:12:20 +0000

    set colour scheme and added login and profile icon to navbar

--- src/components/navigation/nav-bar.tsx
+++ src/components/navigation/nav-bar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LoginIcon from '@mui/icons-material/Login';
 
-import { useAuthenticationContext } from '../../context/authentication-context';
 import { UserSession } from '../../apis/authentication/authentication-api';
 
 export interface NavBarProps {
@@ -18,7 +18,7 @@ export const NavBar: React.FunctionComponent<NavBarProps> = ({ title, userSessio
         <Typography variant='h5' noWrap>
           {title}
         </Typography>
-        {userSession?.isValid() && <AccountCircleIcon sx={{ marginLeft: 'auto' }} />}
+        <IconButton sx={{ marginLeft: 'auto' }}>{userSession ? <AccountCircleIcon /> : <LoginIcon />}</IconButton>
       </Toolbar>
     </AppBar>
   );
--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,6 @@
 import { createTheme } from '@mui/material';
+import { orange, teal } from '@mui/material/colors';
 
-export const theme = createTheme({});
+export const theme = createTheme({
+  palette: { primary: orange, secondary: teal }
+});
